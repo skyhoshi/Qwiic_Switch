a539a652106d4899174074f02a3dfc923e7cd060
pmakin-png <priyanka.makin@sparkfun.com>
2019-09-11 12:05:44 -0600

    Fixed start i2C bug
    Originally we were calling startI2C everytime recordSystemSettings was called (loop()). Now, we only call startI2C after checking if the I2C address has actually been changed.

--- Firmware/Qwiic_Button/Qwiic_Button.ino
+++ Firmware/Qwiic_Button/Qwiic_Button.ino
@@ -148,8 +148,6 @@ LEDconfig onboardLED; //init the onboard LED
 
 void setup(void)
 {
-
-
   pinMode(addressPin0, INPUT_PULLUP); //Internally pull up address pins
   pinMode(addressPin1, INPUT_PULLUP);
   pinMode(addressPin2, INPUT_PULLUP);
@@ -215,6 +213,7 @@ void loop(void)
 {
   if (updateFlag == true)
   {
+    
     //Calculate LED values based on pulse settings if anything has changed
     onboardLED.update(&registerMap);
 
@@ -360,21 +359,33 @@ void readSystemSettings(memoryMap *map)
 //If the current setting is different from that in EEPROM, update EEPROM
 void recordSystemSettings(memoryMap *map)
 {
+  //I2C address is byte
+  byte i2cAddr;
+
   //Error check the current I2C address
-  if (map->i2cAddress < 0x08 || map->i2cAddress > 0x77)
+  if (map->i2cAddress >= 0x08 && map->i2cAddress <= 0x77)
   {
-    //User has set the address out of range
-    //Go back to defaults
-    map->i2cAddress = DEFAULT_I2C_ADDRESS;
+    //Address is valid
+    
+    //Read the value currently in EEPROM. If it's different from the memory map then record the memory map value to EEPROM.
+    EEPROM.get(LOCATION_I2C_ADDRESS, i2cAddr);
+    if (i2cAddr != map->i2cAddress)
+    {
+      EEPROM.put(LOCATION_I2C_ADDRESS, (byte)map->i2cAddress);
+      startI2C(map); //Determine the I2C address we should be using and begin listening on I2C bus
+    }
+  }
+  else
+  {
+    EEPROM.get(LOCATION_I2C_ADDRESS, i2cAddr);
+    map->i2cAddress == i2cAddr; //Return to original address    
   }
 
-  EEPROM.put(LOCATION_I2C_ADDRESS, map->i2cAddress);
+
   EEPROM.put(LOCATION_INTERRUPTS, map->interruptConfig.byteWrapped);
   EEPROM.put(LOCATION_LED_BRIGHTNESS, map->ledBrightness);
   EEPROM.put(LOCATION_LED_PULSEGRANULARITY, map->ledPulseGranularity);
   EEPROM.put(LOCATION_LED_PULSECYCLETIME, map->ledPulseCycleTime);
   EEPROM.put(LOCATION_LED_PULSEOFFTIME, map->ledPulseOffTime);
   EEPROM.put(LOCATION_BUTTON_DEBOUNCE_TIME, map->buttonDebounceTime);
-
-  startI2C(map); //Determine the I2C address we should be using and begin listening on I2C bus
 }

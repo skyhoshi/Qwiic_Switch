814b5298083592d5f3a674b46a6a4d6ee6d9509b
Nathan Seidle <nathan.seidle@sparkfun.local>
2020-01-16 14:49:39 -0700

    Increase programming speed to 1MHz

--- Firmware/program_button.bat
+++ Firmware/program_button.bat
@@ -6,16 +6,15 @@
 @echo Flashing bootloader...
 @avrdude -C avrdude.conf -pattiny84 -cusbtiny -e -Uefuse:w:0xFF:m -Uhfuse:w:0b11010111:m -Ulfuse:w:0xE2:m
 
-
-@echo -
-@echo Erasing EEPROM...
-@avrdude -C avrdude.conf -pattiny84 -cusbtiny -Uflash:w:EEPROM_erase.ino.hex:i
-
 @timeout 1
 
 @echo -
 @echo Flashing firmware...
-@avrdude -C avrdude.conf -pattiny84 -cusbtiny -e -Uflash:w:Qwiic_Button.ino.hex:i
+
+rem The -B1 option reduces the bitclock period (1us = 1MHz SPI), decreasing programming time
+rem May increase verification errors
+
+@avrdude -C avrdude.conf -pattiny84 -cusbtiny -e -Uflash:w:Qwiic_Button.ino.hex:i -B1
 
 @echo Done programming! Move on to next board.
 @pause
